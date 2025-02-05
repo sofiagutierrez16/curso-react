02f6fe5a7722588c208aae422104152cb02d83f7
Sofia <gutierrez.sofiamaite@iesconcepcion.edu.ar>
2025-02-05 20:16:41 -0300

    iniciando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Logos } from './components/Logos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Logos />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
